d9e8285d646ff603c90790a2db91d5ecc6a3e5d7
uzakbekov esentur <esentur_03@mail.ru>
2024-06-08 01:26:26 +0600

    обновил компонент app для отображения user списка

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import React, { useState } from 'react';
+import ToolBar from "./components/ToolBar/ToolBar"
+import UserForm from "./components/UserForm/UserForm"
+import Users from "./components/Users/Users"
+import { User } from './type';
 
 
 
 const App = () => {
 
- 
+  const [users, setUsers] = useState<User[]>([]);
+
+  const addUser = (user: User) => {
+    setUsers([...users, user]);
+  };
+
 
   return (
     <>
-    
+      <header>
+        <ToolBar />
+      </header>
+      <main className="container-fluid">
+        <div className="row mt-2">
+          <div className="col-4">
+            <UserForm onAddUser={addUser} />
+          </div>
+          <div className="col-6">
+            <Users users={users} />
+          </div>
+        </div>
+      </main>
+
     </>
   )
 }
